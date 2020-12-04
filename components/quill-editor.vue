269a31e7e89e0fbd3d65e47f77f88cf8ceb0b7e5
comfuture <comfuture@gmail.com>
2020-12-04 13:46:13 +0900

    Initial commit

---
+++ components/quill-editor.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="ql-container"><div ref="editor"></div></div>
+</template>
+<script>
+export default {
+  name: "quill-editor",
+  props: {
+    value: String,
+    placeholder: String
+  },
+  data() {
+    return {
+      content: ""
+    };
+  },
+  mounted() {
+    Promise.all([
+      import("quill"),
+      import("quill/dist/quill.core.css"),
+      import("quill/dist/quill.snow.css")
+    ]).then(([{ default: Quill }, _, __]) => {
+      var quill = (this.quill = new Quill(this.$refs.editor, {
+        theme: "snow",
+        placeholder: this.placeholder,
+        modules: {
+          toolbar: [
+            // [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+            ["bold", "italic", "underline", "strike"],
+            [{ font: [] }],
+            [{ color: [] }, { background: [] }],
+            [{ align: [] }],
+            // ['blockquote', 'code-block'],
+            [{ list: "ordered" }, { list: "bullet" }],
+            [{ indent: "-1" }, { indent: "+1" }],
+            ["clean"],
+            ["link", "image", "video"]
+          ]
+        }
+      }));
+      quill.pasteHTML(this.value);
+      quill.on("text-change", (delta, oldDelta, source) => {
+        let html = this.$refs.editor.children[0].innerHTML;
+        const text = quill.getText();
+        if (html === "<p><br></p>") html = "";
+        this.content = html;
+        this.$emit("input", this.content);
+        this.$emit("change", { html, text, quill });
+      });
+      quill.focus();
+    });
+  },
+  watch: {
+    value(newVal, oldVal) {
+      if (this.quill) {
+        if (newVal && newVal !== this.content) {
+          this.content = newVal;
+          this.quill.pasteHTML(newVal);
+        } else if (!newVal) {
+          this.quill.setText("");
+        }
+      }
+    }
+  }
+};
+</script>
+<style>
+.ql-container .ql-snow {
+  border: 1px solid rgba(34, 36, 38, 0.15);
+}
+.ql-toolbar {
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+}
+.ql-container {
+  border-bottom-left-radius: 5px;
+  border-bottom-right-radius: 5px;
+}
+.ql-editor {
+  min-height: 200px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <article><quill-editor v-model="content" /></article>
+  </section>
+</template>
+
+<script>
+import QuillEditor from "~/components/quill-editor.vue";
+
+export default {
+  components: {
+    QuillEditor,
+  },
+  data() {
+    return {
+      content: "Hello, world",
+    };
+  },
+};
+</script>
